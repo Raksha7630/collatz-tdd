29180f44588a31c59d0a8c33cd8a9f43b2e0b51b
Raksha7630 <stharakshya63@gmail.com>
2025-09-18 12:04:51 +0300

    testing with AAA pattern

--- tests/test_collatz.py
+++ tests/test_collatz.py
@@ -2,3 +2,20 @@
 
 #I am Rakshya Shrestha.
 #I am testing the collatz function from collatz.py file.
+
+import pytest 
+from src.collatz import collatz_sequence
+
+def test_collatz_example():
+    #Following AAA pattern.
+    #Arrange
+    n = 12 #input value
+    expected = [12, 6, 3, 10, 5, 16, 8, 4, 2, 1]
+
+    #Act
+    result = collatz_sequence(n)
+
+    #Assert
+    assert result == expected
+
+    
